ae448a4acee6fae24afdc3d73ff615a9e402065a
Harry Garrood <hdgarrood@gmail.com>
2012-09-16 19:20:21 +0100

    reshuffle

--- 14/Euler14.hs
+++ 14/Euler14.hs
@@ -1,13 +1,17 @@
+import Data.Function
+import Data.List
+
 collatz :: Int -> [Int]
 collatz n
     | n == 1    =   [1]
     | even n    =   n : collatz (n `div` 2)
     | otherwise =   n : collatz (3*n + 1)
 
-euler14 :: Int
+euler14 :: (Int, Int)
 euler14 = let starts = [1,2..999999]
-              results = map (length . collatz) starts
-          in maximum results 
+              lengths = map (length . collatz) starts
+              results = zip starts lengths
+          in maximumBy (compare `on` snd) results 
 
 main :: IO ()
 main = putStrLn (show euler14)
